f0c513f5ca90f7f9920f861fc203bc40946e6bc4
Jéssica Santos <jessica.js1306@gmail.com>
2025-02-25 15:43:01 -0300

    Spring MVC Form Data Binding - Drop-Down Lists

--- 01-thymeleafdemo-helloword/src/main/java/com/irkaraz/springboot/thymeleafdemo/controller/StudentController.java
+++ 01-thymeleafdemo-helloword/src/main/java/com/irkaraz/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,19 +1,28 @@
 package com.irkaraz.springboot.thymeleafdemo.controller;
 
 import com.irkaraz.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
 
         theModel.addAttribute("student", new Student());
+
+        // add the list of countries to the model
+        theModel.addAttribute("countries", countries);
 
         return "student-form";
     }
